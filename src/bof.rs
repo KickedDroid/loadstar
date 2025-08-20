d0fb5681b516dc5d00134628e5b18ce0898f64e0
Kicked Droid <24928676+KickedDroid@users.noreply.github.com>
2025-08-20 18:27:07 -0500

    Update bof.rs

--- src/bof.rs
+++ src/bof.rs
@@ -1,13 +1,5 @@
 use core::ffi::{c_char, c_int, c_void};
-use utils::resolve_func;
-use utils::{output, printf, GetProcAddress, LoadLibraryA};
-
-type MessageBoxA = unsafe extern "system" fn(
-    hwnd: *mut c_void,
-    test: *mut u8,
-    caption: *mut u8,
-    style: u32,
-) -> isize;
+use utils::output;
 
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn rust_bof(args: *mut c_char, alen: c_int) {
