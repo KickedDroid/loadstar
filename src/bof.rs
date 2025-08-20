0f1d69de25cb88cd04cb35a001bced723f32c2df
Kicked Droid <24928676+KickedDroid@users.noreply.github.com>
2025-08-20 18:26:24 -0500

    Update bof.rs

--- src/bof.rs
+++ src/bof.rs
@@ -11,22 +11,5 @@ type MessageBoxA = unsafe extern "system" fn(
 
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn rust_bof(args: *mut c_char, alen: c_int) {
-    let some_func = resolve_func("user32.dll\0", "MessageBoxA\0");
-    if !some_func.is_null() {
-        let msgbox: MessageBoxA = unsafe { core::mem::transmute(some_func) };
-
-        match (msgbox)(
-            core::ptr::null_mut(),
-            b"Hello bruh\0".as_ptr() as *mut u8,
-            b"Rusty BOF\0".as_ptr() as *mut u8,
-            1,
-        ) as i32
-        {
-            1 => output("you clicked ok"),
-            2 => output("You clicked cancel"),
-            _ => output("exited"),
-        }
-    } else {
-        output("[!] Could not find func");
-    }
+    output("HELOOOOOO");
 }
